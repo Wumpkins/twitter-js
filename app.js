c25333dee2ee1ffdce50c8c6d9fcef97b8422e1e
Wumpkins <dhc2129@columbia.edu>
2015-02-22 20:50:21 -0500

    barebones template

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ var morgan = require('morgan');
 
 var app = express();
 
+var swig = require('swig');
+
+swig.setDefaults({cache: false});
+
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/public/views');
+
 app.use(morgan('dev'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+	res.render( 'index', {title: 'Hall of Fame', people: people} );
 })
 
 var server = app.listen(3000, function(){
